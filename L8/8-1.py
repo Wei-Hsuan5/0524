531831f4cf7514b5970ae9b73fb1e57fb5507168
Wei-Hsuan5 <hsuan12588@gmail.com>
2025-05-24 14:12:09 +0800

    module

---
+++ L8/8-1.py
@@ -0,0 +1,16 @@
+import tools
+
+
+def main():
+   height:int=int(input("身高(cm):"))
+   weight:int=int(input("體重(kg):"))
+   
+   bmi=tools.calculate_bmi(height, weight)
+   
+   print(bmi)
+   
+   print(tools.get_state(bmi))
+      
+
+if __name__ == '__main__':
+   main()
